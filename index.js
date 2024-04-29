f26c51132d26304f36611b523134c1028ea06c57
LucasBrea1 <Lucasbreaxd@gmail.com>
2024-04-29 13:23:24 -0300

    otros archivos como index.js y index.html, librerias agregadas

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path'); // Import path module
+
+const app = express();
+const port = 43922;
+
+// Allow all cross-origin requests
+app.use(cors());
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+/*
+------------------
+Aca van los endpoints.
+------------------
+*/
+
+// Nuestra aplicación esta lista para recibir peticiones
+app.listen(port, () => {
+console.log(`Server is running on http://localhost:${port
+});
+
